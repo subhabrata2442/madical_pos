02f1b7c6b7f2f108d7d10bd3b7b5997ed7e3aeae
asimd2472 <asimd2472@gmail.com>
2023-12-04 11:56:37 +0530

    report error fix

--- resources/views/admin/report/near_expiry_stock.blade.php
+++ resources/views/admin/report/near_expiry_stock.blade.php
@@ -120,7 +120,7 @@
         <table id="" class="table table-bordered text-nowrap">
 			<thead>
 				<th scope="col">Sl NO.</th>
-				{{-- <th scope="col">Product Name</th> --}}
+				<th scope="col">Store</th>
 				<th scope="col">Brand</th>
 				<th scope="col">Product Barcode</th>
 				<th scope="col">Dosage</th>
@@ -132,7 +132,7 @@
                     @if($purchase->t_qty <= $purchase->product->alert_product_qty)
                         <tr>
                             <td>{{($key+1)}}</td>
-                            {{-- <td>{{$purchase->product->product_name}}</td> --}}
+                            <td>{{$purchase->user->name}}</td>
                             <td>{{$purchase->product->brand}}</td>
                             <td>{{$purchase->product->product_barcode}}</td>
                             <td>{{$purchase->product->dosage_name}}</td>
